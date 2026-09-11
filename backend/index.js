8c1f7522bab021f5f93056c8be25de695a0c8f8e
Harsh Desai <desaiharsh512@gmail.com>
2026-09-11 22:10:28 +0530

    setup nodemon

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req, res)=>{
+    res.send("Server is running");
+})
+
 app.listen(3000, ()=>{
-    console.log("Server is running on port localhost:3000");
+    console.log("Server is running on port http://localhost:3000");
 })
