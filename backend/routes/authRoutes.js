ba9046b0c4a62a3b46ade04a1a4e70f0de1bd073
Harsh Desai <desaiharsh512@gmail.com>
2026-09-17 10:52:31 +0530

    code refactor: seperate controller code from routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,94 +1,9 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-
+const authController = require("../controllers/auth.controllers");
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+router.post("/register", authController.register );
 
-        if(!name || !email || !password) {
-            return res.status(400).json({ message: "Please fill in all fields" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create(
-            { 
-                name,
-                email, 
-                password: hashedPassword 
-            }
-        );
-        const token = jwt.sign(
-            {
-                userId: user._id
-            },
-            process.env.JWT_SECRET,
-            { 
-                expiresIn: "1h" 
-            }
-        );
-        res.status(201).json(
-            {
-                message: "User registered successfully",
-                token,
-                user: {
-                    id: user._id,
-                    name: user.name,
-                    email: user.email
-                }
-            }
-        )
-    } catch (error) {
-    console.error("REGISTER ERROR:", error);
-
-    res.status(500).json({
-        message: "Server error",
-        error: error.message
-    });
-}
-});
-
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-
-    if(!email || !password) {
-        return res.status(400).json({ message: "Please fill in all fields" });
-    }
-
-    const user =await User.findOne({ email });
-    if(!user) {
-        return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if(!isMatch) {
-        return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const token = jwt.sign(
-        {
-            userId: user._id
-        },
-        process.env.JWT_SECRET,
-        { 
-            expiresIn: "1h" 
-        }
-    );
-
-    res.status(200).json(
-        {
-            message: "User logged in successfully",
-            token,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        }
-    );
-});
+router.post("/login", authController.login );
 
 module.exports = router;
